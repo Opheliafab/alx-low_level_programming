49ad58e2461a1432af31c72a2271d857adf94513
Opheliafab <pheliae@gmail.com>
2022-09-29 14:25:41 -0700

    my first correction commit

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,17 +1,17 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * _puts_recursion -function
- * @s: input
- * Return: Always 0
+ * _puts_recursion - check the code for ALX Africa  students.
+ * @s: character of string
+ * Return: Always 0.
  */
-void _puts_recursion(char *s);
+void _puts_recursion(char *s)
 {
-	if (*s == '\0')
-		_putchar('\n')
-	else
+	if (*(s) != '\0')
 	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
+	if (*(s) == '\0')
+		_putchar('\n');
 }
