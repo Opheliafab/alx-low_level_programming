72f53c8ad5995926160edb75f32e8cdd77613e4e
Opheliafab <pheliae@gmail.com>
2022-10-01 15:48:48 -0700

    argc argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - declares the argument count and argument vector
+ * @argc: returns the total number of input written to the program
+ * @argv: returns the actual value of the input written to the program
+ * Return: always 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+
+}
