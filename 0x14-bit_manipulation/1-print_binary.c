e6d8a02be9d2175551e65997d6555a394d36b597
Opheliafab <pheliae@gmail.com>
2022-10-28 15:43:36 -0700

    bit

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,20 +2,25 @@
 #include "main.h"
 #include <math.h>
 /**
- * get_bit - Write a function that returns the value of a bit at a
- * given index.
- * Prototype: int get_bit(unsigned long int n, unsigned int index);
- * @index: is the index, starting from 0 of the bit you want to get
- * @n: bit value in the index.
- * Return: the value of the bit at index index or -1 if an error occured
+ * print_binary - Function that prints the binary representation of a number.
+ * Prototype: void print_binary(unsigned long int n);
+ * @n: number to convert to binary
+ * You are not allowed to use arrays
+ * You are not allowed to use malloc
+ * You are not allowed to use the % or / operators
  */
-int get_bit(unsigned long int n, unsigned int index)
+void print_binary(unsigned long int n)
 {
-	if (index > 63)
-		return (-1);
-	if (index == 0)
+	if (n == 0)
 	{
-		return (n & 1);
+		putchar('0');
+		return;
 	}
-	return (get_bit(n >> 1, index - 1));
+	else if (n == 1)
+	{
+		putchar ('1');
+		return;
+	}
+	print_binary(n >> 1);
+	putchar('0' + (n & 1));
 }
